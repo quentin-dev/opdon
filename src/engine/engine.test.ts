f846c7147c948c933104dfa62ad461f34feff4e3
Quentin Barbarat <q.barbarat@gmail.com>
2026-03-06 00:30:52 +0100

    feat: add basic stepping

--- src/engine/engine.test.ts
+++ src/engine/engine.test.ts
@@ -1,23 +1,26 @@
 import { expect, test } from "bun:test";
-import { ActionType, Engine, Player } from ".";
+import { ActionType, CardStatus, Engine, Player, TurnPhase } from ".";
 
 test("Engine - initializes game state correctly", () => {
     const engine = new Engine();
     const gameState = engine.initializeGame();
 
+    expect(gameState.turnNumber).toBe(0);
+    expect(gameState.turnPhase).toBe(TurnPhase.Refresh);
+
     expect(gameState.activePlayer).toBe(Player.PlayerOne);
     expect(gameState.players[Player.PlayerOne].hand).toEqual([]);
-    expect(gameState.players[Player.PlayerOne].deck).toEqual([{ donAttached: 0 }]);
+    expect(gameState.players[Player.PlayerOne].deck).toEqual([{ donAttached: 0, status: CardStatus.Active }]);
     expect(gameState.players[Player.PlayerOne].discardPile).toEqual([]);
     expect(gameState.players[Player.PlayerOne].field).toEqual([]);
-    expect(gameState.players[Player.PlayerOne].leader).toEqual({ donAttached: 0 });
+    expect(gameState.players[Player.PlayerOne].leader).toEqual({ donAttached: 0, status: CardStatus.Active });
     expect(gameState.players[Player.PlayerOne].life).toEqual([]);
 
     expect(gameState.players[Player.PlayerTwo].hand).toEqual([]);
-    expect(gameState.players[Player.PlayerTwo].deck).toEqual([{ donAttached: 0 }]);
+    expect(gameState.players[Player.PlayerTwo].deck).toEqual([{ donAttached: 0, status: CardStatus.Active }]);
     expect(gameState.players[Player.PlayerTwo].discardPile).toEqual([]);
     expect(gameState.players[Player.PlayerTwo].field).toEqual([]);
-    expect(gameState.players[Player.PlayerTwo].leader).toEqual({ donAttached: 0 });
+    expect(gameState.players[Player.PlayerTwo].leader).toEqual({ donAttached: 0, status: CardStatus.Active });
     expect(gameState.players[Player.PlayerTwo].life).toEqual([]);
 });
 
@@ -27,6 +30,6 @@ test("Engine - processes draw card action correctly", () => {
 
     const afterDrawState = engine.processAction(gameState, ActionType.DrawCard, Player.PlayerOne);
 
-    expect(afterDrawState.players[Player.PlayerOne].hand).toEqual([{ donAttached: 0 }]);
+    expect(afterDrawState.players[Player.PlayerOne].hand).toEqual([{ donAttached: 0, status: CardStatus.Active }]);
     expect(afterDrawState.players[Player.PlayerOne].deck).toEqual([]);
 });
--- src/engine/index.ts
+++ src/engine/index.ts
@@ -9,23 +9,52 @@ export enum ActionType {
     DrawCard = "DRAW_CARD",
 }
 
-export interface Card {
+export enum TurnPhase {
+    Refresh = "REFRESH",
+    Draw = "DRAW",
+    Don = "DON!!",
+    Main = "MAIN",
+    End = "END",
+}
+
+export enum CardStatus {
+    Active = "Active",
+    Rested = "Rested",
+}
+
+export interface CardState {
     donAttached: number;
+    status: CardStatus;
+}
+
+export interface LeaderCardState {
+    donAttached: number;
+    status: CardStatus;
+}
+
+// TODO: Could add validation to ensure state is valid (10 in total)
+export interface DonDeckState {
+    total: number;
+    attached: number;
+    available: number;
+    inCostArea: number;
 }
 
 export interface PlayerState {
-    hand: Card[];
-    deck: Card[];
-    discardPile: Card[];
-    field: Card[];
-    leader: Card;
-    life: Card[];
-    // TODO: Add don ?
+    hand: CardState[];
+    deck: CardState[];
+    discardPile: CardState[];
+    field: CardState[];
+    leader: LeaderCardState;
+    life: CardState[];
+    donDeck: DonDeckState;
 }
 
 export interface GameState {
     activePlayer: Player;
     players: Record<Player, PlayerState>;
+    turnNumber: number;
+    turnPhase: TurnPhase;
 }
 
 export const characters = {
@@ -36,27 +65,50 @@ export class Engine {
     initializeGame(): GameState {
         return {
             activePlayer: Player.PlayerOne,
+            turnNumber: 0,
+            turnPhase: TurnPhase.Refresh,
             players: {
                 [Player.PlayerOne]: {
                     hand: [],
-                    deck: [{ donAttached: 0 }],
+                    deck: [{ donAttached: 0, status: CardStatus.Active }],
                     discardPile: [],
                     field: [],
-                    leader: { donAttached: 0 },
+                    leader: { donAttached: 0, status: CardStatus.Active },
                     life: [],
+                    donDeck: { total: 10, attached: 0, available: 0, inCostArea: 0 },
                 },
                 [Player.PlayerTwo]: {
                     hand: [],
-                    deck: [{ donAttached: 0 }],
+                    deck: [{ donAttached: 0, status: CardStatus.Active }],
                     discardPile: [],
                     field: [],
-                    leader: { donAttached: 0 },
+                    leader: { donAttached: 0, status: CardStatus.Active },
                     life: [],
+                    donDeck: { total: 10, attached: 0, available: 0, inCostArea: 0 },
                 },
             },
         };
     }
 
+    // TODO: Test all of this
+    stepToNextPhase(gameState: GameState): GameState {
+        switch (gameState.turnPhase) {
+            case TurnPhase.Refresh:
+                return { ...this.refreshCards(gameState, gameState.activePlayer), turnPhase: TurnPhase.Draw };
+            case TurnPhase.Draw:
+                return { ...this.applyDrawStep(gameState, gameState.activePlayer), turnPhase: TurnPhase.Don };
+            case TurnPhase.Don:
+                return { ...this.distributeDon(gameState, gameState.activePlayer), turnPhase: TurnPhase.Main };
+            case TurnPhase.Main:
+                return { ...gameState, turnPhase: TurnPhase.End };
+            case TurnPhase.End:
+                return { ...gameState, turnPhase: TurnPhase.Refresh, turnNumber: gameState.turnNumber + 1 };
+        }
+        return gameState;
+    }
+
+    // Should return GameState + whether there was an error or not
+    // Only used during main phase ?
     processAction(gameState: GameState, action: ActionType, player: Player): GameState {
         switch (action) {
             case ActionType.DrawCard:
@@ -67,6 +119,43 @@ export class Engine {
         }
 
         return gameState;
+    }
+
+    private refreshCards(gameState: GameState, player: Player): GameState {
+        // Reset all attached dons + reset all rested cards to active
+        const playerState = gameState.players[player];
+
+        for (const card of playerState.field) {
+            card.donAttached = 0;
+            card.status = CardStatus.Active;
+        }
+
+        playerState.leader.donAttached = 0;
+        playerState.leader.status = CardStatus.Active;
+
+        playerState.donDeck.attached = 0;
+        playerState.donDeck.inCostArea = playerState.donDeck.available;
+
+        return gameState;
+    }
+
+    private distributeDon(gameState: GameState, player: Player): GameState {
+        const playerState = gameState.players[player];
+
+        if (playerState.donDeck.available === 10) return gameState;
+
+        const donsToDistribute = gameState.turnNumber === 0 ? 1 : 2;
+
+        playerState.donDeck.available = Math.min(10, playerState.donDeck.available + donsToDistribute);
+        playerState.donDeck.inCostArea = playerState.donDeck.available;
+
+        return gameState;
+    }
+
+    private applyDrawStep(gameState: GameState, player: Player): GameState {
+        if (gameState.turnNumber === 0) return gameState;
+
+        return this.drawCard(gameState, player);
     }
 
     private drawCard(gameState: GameState, player: Player): GameState {
