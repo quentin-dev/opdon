904f15349de10a05accec7fe43ab0983bdcbfd60
Quentin Barbarat <q.barbarat@gmail.com>
2026-03-05 19:41:48 +0100

    feat: add sample tests

---
+++ src/engine/engine.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from "bun:test";
+import { ActionType, Engine, Player } from ".";
+
+test("Engine - initializes game state correctly", () => {
+    const engine = new Engine();
+    const gameState = engine.initializeGame();
+
+    expect(gameState.activePlayer).toBe(Player.PlayerOne);
+    expect(gameState.players[Player.PlayerOne].hand).toEqual([]);
+    expect(gameState.players[Player.PlayerOne].deck).toEqual([{ donAttached: 0 }]);
+    expect(gameState.players[Player.PlayerOne].discardPile).toEqual([]);
+    expect(gameState.players[Player.PlayerOne].field).toEqual([]);
+    expect(gameState.players[Player.PlayerOne].leader).toEqual({ donAttached: 0 });
+    expect(gameState.players[Player.PlayerOne].life).toEqual([]);
+
+    expect(gameState.players[Player.PlayerTwo].hand).toEqual([]);
+    expect(gameState.players[Player.PlayerTwo].deck).toEqual([{ donAttached: 0 }]);
+    expect(gameState.players[Player.PlayerTwo].discardPile).toEqual([]);
+    expect(gameState.players[Player.PlayerTwo].field).toEqual([]);
+    expect(gameState.players[Player.PlayerTwo].leader).toEqual({ donAttached: 0 });
+    expect(gameState.players[Player.PlayerTwo].life).toEqual([]);
+});
+
+test("Engine - processes draw card action correctly", () => {
+    const engine = new Engine();
+    const gameState = engine.initializeGame();
+
+    const afterDrawState = engine.processAction(gameState, ActionType.DrawCard, Player.PlayerOne);
+
+    expect(afterDrawState.players[Player.PlayerOne].hand).toEqual([{ donAttached: 0 }]);
+    expect(afterDrawState.players[Player.PlayerOne].deck).toEqual([]);
+});
--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,5 +1,87 @@
 import { Usopp } from "./game/cards/ST-01/characters/002-Usopp";
 
+export enum Player {
+    PlayerOne = 0,
+    PlayerTwo = 1,
+}
+
+export enum ActionType {
+    DrawCard = "DRAW_CARD",
+}
+
+export interface Card {
+    donAttached: number;
+}
+
+export interface PlayerState {
+    hand: Card[];
+    deck: Card[];
+    discardPile: Card[];
+    field: Card[];
+    leader: Card;
+    life: Card[];
+    // TODO: Add don ?
+}
+
+export interface GameState {
+    activePlayer: Player;
+    players: Record<Player, PlayerState>;
+}
+
 export const characters = {
     "ST-01-002": new Usopp(),
 };
+
+export class Engine {
+    initializeGame(): GameState {
+        return {
+            activePlayer: Player.PlayerOne,
+            players: {
+                [Player.PlayerOne]: {
+                    hand: [],
+                    deck: [{ donAttached: 0 }],
+                    discardPile: [],
+                    field: [],
+                    leader: { donAttached: 0 },
+                    life: [],
+                },
+                [Player.PlayerTwo]: {
+                    hand: [],
+                    deck: [{ donAttached: 0 }],
+                    discardPile: [],
+                    field: [],
+                    leader: { donAttached: 0 },
+                    life: [],
+                },
+            },
+        };
+    }
+
+    processAction(gameState: GameState, action: ActionType, player: Player): GameState {
+        switch (action) {
+            case ActionType.DrawCard:
+                return this.drawCard(gameState, player);
+            default:
+                // TODO: Unknown
+                break;
+        }
+
+        return gameState;
+    }
+
+    private drawCard(gameState: GameState, player: Player): GameState {
+        const playerState = gameState.players[player];
+
+        if (playerState.deck.length === 0) {
+            // Probably not the best
+            return gameState;
+        }
+
+        const drawnCard = playerState.deck.pop();
+        if (drawnCard) {
+            playerState.hand.push(drawnCard);
+        }
+
+        return gameState;
+    }
+}
